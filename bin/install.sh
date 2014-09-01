bbc6c399f41e913e088f5bdc1475eb0c87315141
Tomas Tomecek <ttomecek@redhat.com>
2014-09-01 18:34:11 +0200

    vim: clone vundle correctly

--- bin/install.sh
+++ bin/install.sh
@@ -2,7 +2,7 @@
 mkdir -p ~/.dotfiles/
 mkdir -p ~/.vim/bundle/
 
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 git clone https://github.com/TomasTomecek/dotfiles.git ~/.dotfiles/
 
