ed00d28697d53e17992c74c100f9c786fd4fbb1a
Tomas Tomecek <ttomecek@redhat.com>
2014-10-09 15:30:52 +0200

    tmux: order ip addresses

--- bin/tmux_list_if.sh
+++ bin/tmux_list_if.sh
@@ -1,20 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+declare -A addresses
 line=""
 for i in $(ls -1 /sys/class/net) ; do
     if [ "$i" != "lo" ] ; then
         addr=$(ip -4 -o a s $i | awk '{ print $4 }')
         if [ -n "$addr" ] ; then
-            i_clean=$(echo "$i" | sed -e 's/wlp\w*/W/g' -e 's/enp\w*/E/g' -e 's/tun\w+/V/g')
-            if [ "$i_clean" == "W" ] ; then
-                line="$line#[fg=white]$i_clean #[fg=colour33]$addr "
-            fi
-            if [ "$i_clean" == "E" ] ; then
-                line="$line#[fg=white]$i_clean #[fg=green]$addr "
-            fi
-            if [ "$i_clean" == "V" ] ; then
-                line="$line#[fg=white]$i_clean #[fg=colour166]$addr "
-            fi
+            i_clean=$(echo "$i" | sed -e 's/wlp\w*/W/g' -e 's/enp\w*/E/g' -e 's/tun\w*/V/g')
+            addresses+=([$i_clean]=$addr)
         fi
     fi
 done
+if [ -n "${addresses[W]}" ] ; then
+    line="$line#[fg=white]W #[fg=colour33]${addresses[W]} "
+fi
+if [ -n "${addresses[E]}" ] ; then
+    line="$line#[fg=white]E #[fg=green]${addresses[E]} "
+fi
+if [ -n "${addresses[V]}" ] ; then
+    line="$line#[fg=white]V #[fg=colour166]${addresses[V]} "
+fi
 echo $line
