7e7eab13d33a616d16f669c957ea4f002641b253
Tomas Tomecek <ttomecek@redhat.com>
2015-07-15 08:05:03 +0200

    install: check for binaries properly

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-which git 2>/dev/null || echo "Can't find git binary." && exit 1
-which stow 2>/dev/null || echo "Can't find stow binary." && exit 1
+which git 2>/dev/null || {echo "Can't find git binary." && exit 1}
+which stow 2>/dev/null || {echo "Can't find stow binary." && exit 1}
 
 mkdir -p ~/.dotfiles/
 mkdir -p ~/.vim/bundle/
